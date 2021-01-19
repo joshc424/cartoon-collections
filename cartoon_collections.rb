f89df00593fc7fe5f84f48c424be1ab1f7fab30d
Joshua Cho <joshdavecho@protonmail.com>
2021-01-19 00:40:06 -0500

    commit tree 94a0533063bbc01aeb1ced190f916cb02a2b7f41

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) # code an argument here
+  array.each_with_index do |dwarf_name, number|
+    puts "#{number+1}. #{dwarf_name}"
+  end# Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)# code an argument here
+  array.collect do |element|
+    "#{element.capitalize}!" #Your code here
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)# code an argument here
+  short_words.any? {|words| words.length > 4}# Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
